102ad9299d792bda355f638055f5ca2d8de6512c
Alexis <alexisblugo@gmail.com>
2025-04-10 11:05:56 -0300

    se agrega el proyecto

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Document</title>
+
+    <script>
+        tailwind.config = {
+            fontFamily: {
+                sans: ['Poppins', 'sans-serif'],
+            },
+        }
+    </script>
+</head>
+
+<body class="">
+    @yield('content')
+
+    @yield('scripts')
+</body>
+
+</html>
